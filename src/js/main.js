78bddb451efd6f25390082d4af7e4a20b258e5b9
«yura» <avanesov89@mail.ru>
2021-06-14 22:35:08 +0300

    edit files

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
-/**
-  * название функции
-  *
-  * @param {number} first - первое число
-  * @returns {number}
-  */
 
 
+ $(window).on('load', function () {
+  var $preloader = $('#preloader'),
+      $icon_animate = $preloader.find('.icon_animate');
+  $icon_animate.fadeOut();
+  $preloader.delay(300).fadeOut('slow');
+});
 
 
  jQuery(document).ready(function($){
@@ -185,3 +185,7 @@ $(window).scroll(function() {
     $('.scrollup').fadeOut();
   }
 });
+
+
+
+new WOW().init();
